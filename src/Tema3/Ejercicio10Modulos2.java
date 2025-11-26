90fde580291408edc4c2e403df76d85b7b1a5856
Ian-Ian-Ian-Ian-Ian <ianbortom@alu.edu.gva.es>
2025-11-26 10:30:52 +0100

    Commit

--- src/Tema3/Ejercicio10Modulos2.java
+++ src/Tema3/Ejercicio10Modulos2.java
@@ -1,7 +1,166 @@
 package Tema3;
 
-import Tema3.MyMath;
+import java.util.Scanner;
+
+import static Tema3.MyMath.*;
 
 public class Ejercicio10Modulos2 {
+    public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        double perimetro = 0;
+        double area = 0;
+        double base = 0;
+        double altura = 0;
+        double resul = 0;
+        int resulint = 0;
+
+        int opcion = mathMenu();
+        int figura = 0;
+
+        if (opcion == 1 || opcion == 2) {
+            System.out.println("Selecciona la figura la cual quieres calcular");
+            System.out.println("---------------------------------------------");
+            System.out.println("1. Cuadrado");
+            System.out.println("2. Rectángulo");
+            System.out.println("3. Círculo");
+            figura = in.nextInt();
+            while (figura < 1 || figura > 3) {
+                System.out.println("Selecciona una opción correcta");
+                figura = in.nextInt();
+            }
+        }
+
+        switch (opcion) {
+            case 1:
+                if (figura == 1) {
+                    System.out.println("Dime el lado de la figura");
+                    base = in.nextInt();
+                    resul = squarePerimeter(base);
+                    System.out.println("El perímetro del cuadrado es de " + resul);
+                } else if (figura == 2) {
+                    System.out.println("Dime la base de la figura");
+                    base = in.nextInt();
+                    System.out.println("Y ahora dime la altura de la figura");
+                    altura = in.nextInt();
+                    resul = rectanglePerimeter(base, altura);
+                    System.out.println("El perímetro del rectángulo es de " + resul);
+                } else if (figura == 3) {
+                    System.out.println("Dime el radio del círculo");
+                    base = in.nextInt();
+                    resul = circlePerimeter(base);
+                    System.out.println("El perímetro del círculo es de " + resul);
+                }
+                break;
+            case 2:
+                if (figura == 1) {
+                    System.out.println("Dime el lado de la figura");
+                    base = in.nextInt();
+                    resul = squareArea(base);
+                    System.out.println("El área del cuadrado es de " + resul);
+                } else if (figura == 2) {
+                    System.out.println("Dime la base de la figura");
+                    base = in.nextInt();
+                    System.out.println("Y ahora dime la altura de la figura");
+                    altura = in.nextInt();
+                    resul = rectangleArea(base, altura);
+                    System.out.println("El área del rectángulo es de " + resul);
+                } else if (figura == 3) {
+                    System.out.println("Dime el radio del círculo");
+                    base = in.nextInt();
+                    resul = circleArea(base);
+                    System.out.println("El área del círculo es de " + resul);
+                }
+                break;
+            case 3:
+                System.out.println("Dime el número");
+                resulint = in.nextInt();
+                boolean primo = primeNumber(resulint);
+                if (primo) {
+                    System.out.println("El número es primo");
+                } else if (!primo) {
+                    primo = notPrimeNumber(resulint);
+                    System.out.println("El número NO es primo");
+                }
+                break;
+            case 4:
+                System.out.println("Dime el número");
+                resulint = in.nextInt();
+                resulint = digitNumber(resulint);
+                if (resulint == 1) {
+                    System.out.println("El número tiene " + resulint + " dígito");
+                } else {
+                    System.out.println("El número tiene " + resulint + " dígitos");
+                }
+                break;
+            case 5:
+                System.out.println("Dime el número");
+                resulint = in.nextInt();
+                resulint = evenDigits(resulint);
+                if (resulint == 1) {
+                    System.out.println("El número tiene " + resulint + " dígito par");
+                } else {
+                    System.out.println("El número tiene " + resulint + " dígitos pares");
+                }
+                break;
+            case 6:
+                System.out.println("Dime el número");
+                resulint = in.nextInt();
+                resulint = oddDigits(resulint);
+                if (resulint == 1) {
+                    System.out.println("El número tiene " + resulint + " dígito impar");
+                } else {
+                    System.out.println("El número tiene " + resulint + " dígitos impares");
+                }
+                break;
+            case 7:
+                System.out.println("Dime el número del cuál quieres calcular el factorial");
+                resulint = in.nextInt();
+                if (resulint < 0) {
+                    resulint = -resulint;
+                } else if (resulint == 0 || resulint == 1) {
+                    resulint = 1;
+                } else {
+                    resulint = factorialNumber(resulint);
+                }
+                System.out.println("El resultado es: " + resulint);
+                break;
+            case 8:
+                System.out.println("Dime el número del cuál quieres calcular el factorial");
+                resulint = in.nextInt();
+                if (resulint < 0) {
+                    resulint = -resulint;
+                } else if (resulint == 0 || resulint == 1) {
+                    resulint = 1;
+                } else {
+                    resulint = recursiveFactorialNumber(resulint);
+                }
+                System.out.println("El resultado es: " + resulint);
+                break;
+            case 9:
+                System.out.println("Dime el primer coeficiente");
+                int a = in.nextInt();
+                System.out.println("Dime el segundo coeficiente");
+                int b = in.nextInt();
+                System.out.println("Dime el tercer coeficiente");
+                int c = in.nextInt();
+                resulint = equation(a, b, c);
+
+                if (resulint > 0 ) {
+                    System.out.println("Tiene dos soluciones reales");
+                } else if (resulint == 0) {
+                    System.out.println("Tiene una solución real");
+                } else {
+                    System.out.println("No tiene solución real");
+                }
+                break;
+            case 10:
+                System.out.println("Dime el número");
+                resulint = in.nextInt();
+                resulint = digitCount(resulint);
+                System.out.println("El total de los dígitos es " + resulint);
+                break;
+        }
+    }
 }
+
--- src/Tema3/MyMath.java
+++ src/Tema3/MyMath.java
@@ -1,7 +1,6 @@
 package Tema3;
 
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 public class MyMath {
     public static int mathMenu() {
@@ -20,13 +19,14 @@ public class MyMath {
         System.out.println("10.Suma de los dígitos");
         int opcion = in.nextInt();
 
-            while (opcion < 1 || opcion > 10) {
-                System.out.println("Selecciona una opción correcta");
-                opcion = in.nextInt();
-            }
+        while (opcion < 1 || opcion > 10) {
+            System.out.println("Selecciona una opción correcta");
+            opcion = in.nextInt();
+        }
         return opcion;
     }
 
+    // -------------------------------------------------------------------------------------------------------
     public static double squarePerimeter(double lado) {
         return lado * 4;
     }
@@ -50,16 +50,17 @@ public class MyMath {
     public static double circleArea(double radio) {
         return Math.PI * (radio * radio);
     }
+    // -------------------------------------------------------------------------------------------------------
 
-    public static boolean primeNumber (int numero) {
+    public static boolean primeNumber(int numero) {
         int divisores = 0;
         int dividendo = numero;
         int resultado = 0;
         boolean verdad = false;
 
-        while (dividendo > 0){
-            resultado = numero%dividendo;
-            if (resultado == 0){
+        while (dividendo > 0) {
+            resultado = numero % dividendo;
+            if (resultado == 0) {
                 divisores++;
             }
             dividendo--;
@@ -71,41 +72,43 @@ public class MyMath {
         return verdad;
     }
 
-    public static boolean notPrimeNumber (int numero) {
+    public static boolean notPrimeNumber(int numero) {
         return !primeNumber(numero);
     }
+
+    // -------------------------------------------------------------------------
 
     public static int digitNumber(int numero) {
         int cifra = 0;
         while (numero != 0) {
-            numero = numero/10;
+            numero = numero / 10;
             cifra++;
         }
         return cifra;
     }
 
-    public static int evenDigits(int numero){
+    public static int evenDigits(int numero) {
         int par = 0;
         int cifra = 0;
-            while (numero != 0) {
-                cifra = numero%2;
-                if  (cifra == 0){
-                    par++;
-                }
-                numero = numero/10;
+        while (numero != 0) {
+            cifra = numero % 2;
+            if (cifra == 0) {
+                par++;
             }
+            numero = numero / 10;
+        }
         return par;
     }
 
-    public static int oddDigits(int numero){
+    public static int oddDigits(int numero) {
         int impar = 0;
         int cifra = 0;
         while (numero != 0) {
-            cifra = numero%2;
-            if  (cifra == 1){
+            cifra = numero % 2;
+            if (cifra == 1) {
                 impar++;
             }
-            numero = numero/10;
+            numero = numero / 10;
         }
         return impar;
     }
@@ -127,170 +130,30 @@ public class MyMath {
     }
 
     public static int equation(int a, int b, int c) {
-        return (b*2) - (4 * a * c);
+        return (b * 2) - (4 * a * c);
     }
 
-    private static int digitCount(int numero) {
-        int total = 0;
-            numero = 0;
+    public static int digitCount(int numero) {
+        int total = numero;
+        int repet = 0;
+        int digit = 1;
+
+        while (total > 9) {
+            total = total / 10;
+            repet++;
+        }
+        total = 0;
+        while (repet > 0) {
+            digit = digit * 10;
+            repet--;
+        }
+        while (numero > 0) {
+            while (numero >= digit) {
+                numero = numero - digit;
+                total++;
+            }
+            digit = digit / 10;
+        }
         return total;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        double perimetro = 0;
-        double area = 0;
-        double base = 0;
-        double altura = 0;
-        double resul = 0;
-        int resulint = 0;
-
-        int opcion = mathMenu();
-        int figura = 0;
-
-        if (opcion == 1 || opcion == 2) {
-            System.out.println("Selecciona la figura la cual quieres calcular");
-            System.out.println("---------------------------------------------");
-            System.out.println("1. Cuadrado");
-            System.out.println("2. Rectángulo");
-            System.out.println("3. Círculo");
-            figura = in.nextInt();
-                while (figura < 1 || figura > 3) {
-                    System.out.println("Selecciona una opción correcta");
-                    figura = in.nextInt();
-                }
-        }
-
-        switch (opcion) {
-            case 1:
-                if (figura == 1) {
-                    System.out.println("Dime el lado de la figura");
-                    base = in.nextInt();
-                    resul = squarePerimeter(base);
-                    System.out.println("El perímetro del cuadrado es de " + resul);
-                } else if (figura == 2) {
-                    System.out.println("Dime la base de la figura");
-                    base = in.nextInt();
-                    System.out.println("Y ahora dime la altura de la figura");
-                    altura = in.nextInt();
-                    resul = rectanglePerimeter(base, altura);
-                    System.out.println("El perímetro del rectángulo es de " + resul);
-                } else if (figura == 3) {
-                    System.out.println("Dime el radio del círculo");
-                    base = in.nextInt();
-                    resul = circlePerimeter(base);
-                    System.out.println("El perímetro del círculo es de " + resul);
-                }
-                break;
-            case 2:
-                if (figura == 1) {
-                    System.out.println("Dime el lado de la figura");
-                    base = in.nextInt();
-                    resul = squareArea(base);
-                    System.out.println("El área del cuadrado es de " + resul);
-                } else if (figura == 2) {
-                    System.out.println("Dime la base de la figura");
-                    base = in.nextInt();
-                    System.out.println("Y ahora dime la altura de la figura");
-                    altura = in.nextInt();
-                    resul = rectangleArea(base, altura);
-                    System.out.println("El área del rectángulo es de " + resul);
-                } else if (figura == 3) {
-                    System.out.println("Dime el radio del círculo");
-                    base = in.nextInt();
-                    resul = circleArea(base);
-                    System.out.println("El área del círculo es de " + resul);
-                }
-                break;
-            case 3:
-                System.out.println("Dime el número");
-                resulint = in.nextInt();
-                boolean primo = primeNumber(resulint);
-                if (primo) {
-                    System.out.println("El número es primo");
-                } else if (!primo) {
-                    primo = notPrimeNumber(resulint);
-                    System.out.println("El número NO es primo");
-                }
-                break;
-            case 4:
-                System.out.println("Dime el número");
-                resulint = in.nextInt();
-                resulint = digitNumber(resulint);
-                if (resulint == 1) {
-                    System.out.println("El número tiene " + resulint + " dígito");
-                } else {
-                    System.out.println("El número tiene " + resulint + " dígitos");
-                }
-                break;
-            case 5:
-                System.out.println("Dime el número");
-                resulint = in.nextInt();
-                resulint = evenDigits(resulint);
-                if (resulint == 1) {
-                    System.out.println("El número tiene " + resulint + " dígito par");
-                } else {
-                    System.out.println("El número tiene " + resulint + " dígitos pares");
-                }
-                break;
-            case 6:
-                System.out.println("Dime el número");
-                resulint = in.nextInt();
-                resulint = oddDigits(resulint);
-                if (resulint == 1) {
-                    System.out.println("El número tiene " + resulint + " dígito impar");
-                } else {
-                    System.out.println("El número tiene " + resulint + " dígitos impares");
-                }
-                break;
-            case 7:
-                System.out.println("Dime el número del cuál quieres calcular el factorial");
-                resulint = in.nextInt();
-                if (resulint < 0) {
-                    resulint = -resulint;
-                } else if (resulint == 0 || resulint == 1) {
-                    resulint = 1;
-                } else {
-                    resulint = factorialNumber(resulint);
-                }
-                System.out.println("El resultado es: " + resulint);
-                break;
-            case 8:
-                System.out.println("Dime el número del cuál quieres calcular el factorial");
-                resulint = in.nextInt();
-                if (resulint < 0) {
-                    resulint = -resulint;
-                } else if (resulint == 0 || resulint == 1) {
-                    resulint = 1;
-                } else {
-                    resulint = recursiveFactorialNumber(resulint);
-                }
-                System.out.println("El resultado es: " + resulint);
-                break;
-            case 9:
-                System.out.println("Dime el primer coeficiente");
-                int a = in.nextInt();
-                System.out.println("Dime el segundo coeficiente");
-                int b = in.nextInt();
-                System.out.println("Dime el tercer coeficiente");
-                int c = in.nextInt();
-                resulint = equation(a, b, c);
-
-                if (resulint > 0 ) {
-                    System.out.println("Tiene dos soluciones reales");
-                } else if (resulint == 0) {
-                    System.out.println("Tiene una solución real");
-                } else {
-                    System.out.println("No tiene solución real");
-                }
-                break;
-            case 10:
-                System.out.println("Dime el número");
-                resulint = in.nextInt();
-                resulint = digitCount(resulint);
-                System.out.println("El total de los dígitos es " + resulint);
-                break;
-        }
     }
 }
