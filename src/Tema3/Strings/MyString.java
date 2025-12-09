7d583d0b5e828458298d221aed3603b849ca0810
Ian-Ian-Ian-Ian-Ian <ianbortom@alu.edu.gva.es>
2025-12-09 12:12:25 +0100

    Commit

---
+++ src/Tema3/Strings/MyString.java
@@ -0,0 +1,28 @@
+package Tema3.Strings;
+
+import java.util.Scanner;
+
+public class MyString {
+
+    public static String stringInverter(String cadena) {
+        int lenght = cadena.length() - 1;
+        char num1 = cadena.charAt(0);
+        char num2 = cadena.charAt(lenght);
+        return cadena;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String cadena = "murcielago";
+        int opcion = 1;
+
+        switch (opcion) {
+            case 1:
+                String cadena2 = stringInverter(cadena);
+                break;
+
+        }
+    }
+}
