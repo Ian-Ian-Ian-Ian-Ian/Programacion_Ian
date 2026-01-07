38f441c818937daec75cc050d2c3315bc6a247ee
Ian-Ian-Ian-Ian-Ian <ianbortom@alu.edu.gva.es>
2026-01-07 13:59:49 +0100

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String name;
+        int age;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Persona persona = new Persona();
+        age = persona.getEdad();
+        name = persona.getNombre();
+        Persona.showInfo(name, age);
+
+        Persona persona1 = new Persona("Manuel");
+        age = persona1.getEdad();
+        Persona.showInfo(name, age);
+
+        Persona persona2 = new Persona("Eduardo", 20);
+        Persona.showInfo(name, age);
     }
 }
