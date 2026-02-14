1ea524a0de34ac136cad451467ad100d319f7aae
posok <ianbortom@alu.edu.gva.es>
2026-02-14 21:01:10 +0100

    Por fin acabo esto

--- src/Tema5/Excepciones/Ejercicio4.java
+++ src/Tema5/Excepciones/Ejercicio4.java
@@ -1,5 +1,7 @@
 package Tema5.Excepciones;
 
+import javax.swing.*;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,17 +12,30 @@ public class Ejercicio4 {
 
         int rng = random.nextInt(100) + 1;
         int[] vector = new int[rng];
-
+        int num = 0;
 
         for (int i = 0; i < vector.length; i++) {
             rng = random.nextInt(10) + 1;
             vector[i] = rng;
         }
 
-        for (int i : vector) {
-            System.out.println(vector[i]);
+        System.out.println("Dime el número de la casilla del vector que quieras buscar entre 1 y " + vector.length);
+        System.out.println("Si quieres acabar, introduce un número negativo");
+        while (num >= 0) {
+            try {
+                num = in.nextInt();
+
+                System.out.println("Número en la casilla " + num + ": " + vector[num - 1]);
+            } catch (ArrayIndexOutOfBoundsException ex1) {
+                if (!(num < 0)) {
+                    System.err.println("No es un número dentro del rango");
+                }
+            } catch (InputMismatchException ex2) {
+                System.err.println("Se ha introducido un dato erróneo, vuelva a intentarlo");
+                in.next();
+            }
         }
 
-
+        System.out.println("Se ha terminado el programa con éxito");
     }
 }
---
+++ src/Tema5/Excepciones/Ejercicio5.java
@@ -0,0 +1,91 @@
+package Tema5.Excepciones;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void showMenu() {
+        System.out.println("Selecciona la opción que quieras realizar");
+        System.out.println("-----------------------------------------");
+        System.out.println("1. Introducir valor positivo e imprimirlo");
+        System.out.println("2. Introducir valor negativo e imprimirlo");
+        System.out.println("3. Salir");
+    }
+
+    public static void imprimePositivo(int p) {
+        if (p < 0) {
+            System.err.println("Se ha introducido un valor negativo, no se printeará el número");
+        } else {
+            System.out.println(p);
+        }
+    }
+
+    public static void imprimeNegativo(int n) {
+        if (n >= 0) {
+            System.err.println("Se ha introfucido un valor positivo, no se printeará el número");
+        } else {
+            System.out.println(n);
+        }
+    }
+
+    public static int exceptionChecker() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Introduce el valor: ");
+        int valor = 0;
+        boolean valido = false;
+
+        while (!valido) {
+            try {
+                valor = in.nextInt();
+                valido = true;
+            } catch (InputMismatchException ex) {
+                System.err.println("Se ha introducido un valor erróneo");
+                in.next();
+                System.out.print("por favor introduzca un valor correcto: ");
+            }
+        }
+
+        return valor;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int valor = 0;
+        int opcion = 0;
+        boolean bucle = true;
+
+        while (bucle) {
+            showMenu();
+            while (bucle) {
+                try {
+                    opcion = in.nextInt();
+                    while (opcion < 1 || opcion > 3) {
+                        System.out.println("Seleccione una opción válida");
+                        opcion = in.nextInt();
+                    }
+                    bucle = false;
+                } catch (InputMismatchException ex) {
+                    System.err.println("Se ha introducido un valor erróneo");
+                    System.out.println("Por favor, seleccione una opción válida");
+                    in.next();
+                }
+            }
+            bucle = true;
+
+            switch (opcion) {
+                case 1:
+                    valor = exceptionChecker();
+                    imprimePositivo(valor);
+                    break;
+                case 2:
+                    valor = exceptionChecker();
+                    imprimeNegativo(valor);
+                    break;
+                case 3:
+                    bucle = false;
+                    break;
+            }
+        }
+    }
+}
